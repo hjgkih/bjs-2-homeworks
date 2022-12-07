46755031d639d809015d248950b7a46ccaebd758
hjgkih <melyanovleg@gmail.com>
2022-12-07 13:11:34 +0300

    Third task is done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
 function compareArrays(arr1, arr2) {
-  
+let array = [arr1, arr2];
+
+let result = array.every(element => element.length === arr2.length && JSON.stringify(element) === JSON.stringify(arr2));
+
+return result;
 }
 
+
+
+
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+   let result = users.filter(user => user.gender === gender).map((user) => user.age).reduce((acc, item, index, arr)=> {
+     acc += item;
+     if (index === arr.length - 1) {
+        return acc / arr.length;
+     }
+
+     return acc;
+   }, 0)
+
+   return result;
 }
