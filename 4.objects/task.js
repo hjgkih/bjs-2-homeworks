2976eafc65d5bcd01503741ee4b835732bd862eb
hjgkih <melyanovleg@gmail.com>
2022-12-12 22:46:39 +0300

    Fourth task is almost done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,44 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+let student1 = new Student();
+let student2 = new Student();
+let student3 = new Student();
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (marks.length !== 0) {
+    for (let i = 0; i < marks.length; i++) {
+      this.marks.push(marks[i])
+    } 
+   }
 }
 
 Student.prototype.getAverage = function () {
-  
+  let result = 0;
+
+  if (this.marks.length === 0) {
+    return 0;
+  } else {
+      for (let i = 0; i < this.marks.length; i++) {
+        result += this.marks[i];
+    }
+    return result / this.marks.length;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+delete this.subject;
+delete this.marks;  
+this.excluded = reason;
 }
+
+
+
