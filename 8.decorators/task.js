2c04cf2f1c7e825c28cfc180b8faf68710b1e895
hjgkih <melyanovleg@gmail.com>
2022-12-29 22:56:14 +0300

    first task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,32 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
+  const cache = [];
+
+  return function (...args) {
+    const hash = md5(args);
+
+    const objectInCache = cache.find(object => object.hash === hash); 
+
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }  
   
+    let value = func(...args);
+    cache.push({hash, value}); 
+    
+    const cacheValueLength = 5;
+
+    if (cache.length > cacheValueLength) { 
+      cache.shift();
+    }
+    
+    console.log("Вычисляем: " + value);
+    return "Вычисляем: " + value;  
+  }
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+
 }
